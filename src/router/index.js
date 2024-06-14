fdc2b58a2d7bdbdb592181905d979b5db87715d0
HIBOY <soapmans@icloud.com>
2024-06-14 22:08:25 +0800

    add router

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,34 @@
+// createRouter: 创建router实例对象
+// createWebHistory: 创建history模式的路由
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import login from '@/views/login/index.vue'
+import layout from '@/views/layout/index.vue'
+import home from '@/views/home/index.vue'
+import category from '@/views/category/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  // path和component对应关系的位置
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      component: layout,
+      children: [
+        {
+          // 默认home页面 path为空
+          path: '',
+          component: home
+        },
+        {
+          path: 'category',
+          component: category
+        }
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/login',
+      component: login 
+    },
   ]
 })
 
